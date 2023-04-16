1b86fb5807c51fb25c1526893eaf6378a42196b6
bartek <smalecbartlomiej@gmail.com>
2023-04-16 14:04:05 +0200

    Configured users microservice to fetch config from config server, added actuator

--- src/main/java/com/bartek/PhotoAppApiUser/PhotoAppApiUserApplication.java
+++ src/main/java/com/bartek/PhotoAppApiUser/PhotoAppApiUserApplication.java
@@ -1,6 +1,8 @@
 package com.bartek.PhotoAppApiUser;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +19,11 @@ public class PhotoAppApiUserApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder(){
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository(){
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
