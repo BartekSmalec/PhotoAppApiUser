642e9cb681e2dc5c017b885f0425cdc92da61bbf
bartek <smalecbartlomiej@gmail.com>
2023-04-20 20:08:25 +0200

    Added AlbumsFallbackFactory

--- src/main/java/com/bartek/PhotoAppApiUser/data/AlbumsServiceClient.java
+++ src/main/java/com/bartek/PhotoAppApiUser/data/AlbumsServiceClient.java
@@ -1,6 +1,10 @@
 package com.bartek.PhotoAppApiUser.data;
 
 import com.bartek.PhotoAppApiUser.model.AlbumResponseModel;
+import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +13,38 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name="albums-ws", fallback = AlbumsFallback.class)
+
+
+@FeignClient(name = "albums-ws", fallbackFactory = AlbumsFallbackFactory.class)
 public interface AlbumsServiceClient {
     @GetMapping("/users/{id}/albums")
     public List<AlbumResponseModel> getAlbums(@PathVariable String id);
 }
+
 @Component
-class AlbumsFallback implements AlbumsServiceClient{
+class AlbumsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
+    @Override
+    public AlbumsServiceClient create(Throwable cause) {
+        return new AlbumsServiceClientFallback(cause);
+    }
+}
+
+class AlbumsServiceClientFallback implements AlbumsServiceClient {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Throwable cause;
+
+    public AlbumsServiceClientFallback(Throwable cause) {
+        this.cause = cause;
+    }
 
     @Override
     public List<AlbumResponseModel> getAlbums(String id) {
+        if(cause instanceof FeignException && ((FeignException) cause).status() == 404){
+            logger.error(cause.getLocalizedMessage());
+        } else {
+            logger.error("Other error took place: " + cause.getLocalizedMessage());
+        }
         return new ArrayList<>();
     }
 }
